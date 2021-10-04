5bda39007585cc1dcefc9963a381683bc0cb82fb
Christopher de Olveira <christopherldo@gmail.com>
2021-10-04 18:19:50 -0400

    Added fetchTodos request

---
+++ src/components/Todos.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="todos">
+      <div class="todo" v-for="todo in allTodos" :key="todo.id">
+        {{ todo.title }}
+      </div>
+    </div>
+  </div>  
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'Todos',
+  computed: {
+    ...mapGetters(['allTodos']),
+  },
+  methods: {
+    ...mapActions(['fetchTodos']),
+  },
+  created() {
+    this.fetchTodos();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .todos {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1rem;
+
+    .todo {
+      border: 1px solid #ccc;
+      background: #41b883;
+      padding: 1rem;
+      border-radius: 5px;
+      text-align: center;
+      position: relative;
+      cursor: pointer;
+    }
+  }
+</style>
---
+++ src/store/modules/todos.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const state = {
+  todos: [],
+};
+
+const getters = {
+  allTodos: state => state.todos,
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos'
+    );
+    
+    commit('setTodos', response.data);
+  }
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
